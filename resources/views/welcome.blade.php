d8d54b03ec66d310eb1a5a0f0061caeb4e7c87a9
Henriy1906 <henriquedeitosdias@gmail.com>
2023-10-17 17:21:56 -0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rockland</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100 p-6">
+    <h1 class="text-3xl font-bold text-center mb-6">ROCKLAND</h1>
+    <div class="max-w-7.5xl mx-auto flex space-x-4">
+        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+            <img src="https://upload.wikimedia.org/wikipedia/pt/6/68/Sgt_Peppers.jpg" alt="" class="w-full rounded-lg mb-4">
+            <h2 class="text-lg font-semibold text-center">Sgt. Pepper's Lonely Hearts Club Band <br>The Beatles</h2>
+        </div>
+        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+            <img src="https://upload.wikimedia.org/wikipedia/pt/3/3b/Dark_Side_of_the_Moon.png" alt="" class="w-full rounded-lg mb-4">
+            <h2 class="text-lg font-semibold text-center">The Dark Side Of The Moon <br>Pink Floyd</h2>
+        </div>
+        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+            <img src="https://upload.wikimedia.org/wikipedia/pt/4/4b/LedZeppelinIV.jpg" alt="" class="w-full rounded-lg mb-4">
+            <h2 class="text-lg font-semibold text-center">Led Zeppelin IV <br>Led Zeppelin</h2>
+        </div>
+        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+            <img src="https://upload.wikimedia.org/wikipedia/pt/5/5b/U2_-_The_Joshua_Tree.png" alt="" class="w-full rounded-lg mb-4">
+            <h2 class="text-lg font-semibold text-center">The Joshua Tree <br>U2</h2>
+        </div>
+        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+            <img src="https://lastfm.freetls.fastly.net/i/u/ar0/4a0123fcd30da2937dc8b9ca4c80f8c8.jpg" alt="" class="w-full rounded-lg mb-4">
+            <h2 class="text-lg font-semibold text-center">Exile on Main St. <br>The Rolling Stones</h2>
+        </div>
+    </div>
+</body>
+</html>
