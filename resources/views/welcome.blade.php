3cfdd956c07057d6032b668855537b08702cf7e3
Henriy1906 <henriquedeitosdias@gmail.com>
2023-10-31 17:44:10 -0300

    mais alterações

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,38 +7,38 @@
     <title>Rockland</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="bg-gray-100 p-6">
+<body class="bg-gray-100 p-10">
     <h1 class="text-3xl font-bold text-center mb-6">ROCKLAND</h1>
-    <div class="max-w-7.5xl mx-auto flex space-x-4">
-        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+    <div class="max-w-7.5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        <div class="p-4 border border-gray-300 rounded-lg">
             <img src="https://upload.wikimedia.org/wikipedia/pt/6/68/Sgt_Peppers.jpg" alt="" class="w-full rounded-lg mb-4">
             <h2 class="text-lg font-semibold text-center">Sgt. Pepper's Lonely Hearts Club Band <br>The Beatles</h2>
         </div>
-        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+        <div class="p-4 border border-gray-300 rounded-lg">
             <img src="https://upload.wikimedia.org/wikipedia/pt/3/3b/Dark_Side_of_the_Moon.png" alt="" class="w-full rounded-lg mb-4">
             <h2 class="text-lg font-semibold text-center">The Dark Side Of The Moon <br>Pink Floyd</h2>
         </div>
-        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+        <div class="p-4 border border-gray-300 rounded-lg">
             <img src="https://upload.wikimedia.org/wikipedia/pt/4/4b/LedZeppelinIV.jpg" alt="" class="w-full rounded-lg mb-4">
             <h2 class="text-lg font-semibold text-center">Led Zeppelin IV <br>Led Zeppelin</h2>
         </div>
-        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+        <div class="p-4 border border-gray-300 rounded-lg">
             <img src="https://upload.wikimedia.org/wikipedia/pt/5/5b/U2_-_The_Joshua_Tree.png" alt="" class="w-full rounded-lg mb-4">
             <h2 class="text-lg font-semibold text-center">The Joshua Tree <br>U2</h2>
         </div>
-        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+        <div class="p-4 border border-gray-300 rounded-lg">
             <img src="https://lastfm.freetls.fastly.net/i/u/ar0/4a0123fcd30da2937dc8b9ca4c80f8c8.jpg" alt="" class="w-full rounded-lg mb-4">
             <h2 class="text-lg font-semibold text-center">Exile on Main St. <br>The Rolling Stones</h2>
-        </div> 
-        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+        </div>
+        <div class="p-4 border border-gray-300 rounded-lg">
             <img src="https://upload.wikimedia.org/wikipedia/en/9/95/Bob_Dylan_-_Highway_61_Revisited.jpg" alt="" class="w-full rounded-lg mb-4">
             <h2 class="text-lg font-semibold text-center">Highway 61 Revisited <br>Bob Dylan</h2>
         </div>
-        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+        <div class="p-4 border border-gray-300 rounded-lg">
             <img src="https://upload.wikimedia.org/wikipedia/pt/1/1c/Pet_Sounds.jpg" alt="" class="w-full rounded-lg mb-4">
             <h2 class="text-lg font-semibold text-center">Pet Sounds <br>Beach Boys</h2>
         </div>
-        <div class="flex-1 p-4 border border-gray-300 rounded-lg">
+        <div class="p-4 border border-gray-300 rounded-lg">
             <img src="https://img2.migalhas.com.br/_MEDPROC_/https__img.migalhas.com.br__SL__gf_base__SL__empresas__SL__MIGA__SL__imagens__SL__2022__SL__01__SL__04__SL__56c5333d-e60d-41b3-a635-cbaa6bd54487.jpg._PROC_CP65.jpg" alt="" class="w-full rounded-lg mb-4">
             <h2 class="text-lg font-semibold text-center">Nevermind <br>Nirvana</h2>
         </div>
