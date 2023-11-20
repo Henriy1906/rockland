283374eb67fbef5575428c47c194e16ca7bfad00
GuizinMEndes2 <126169397+GuizinMEndes2@users.noreply.github.com>
2023-11-20 19:41:57 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/history', function () {
+    return view('history2');
+});
 
 Route::get('/history2', function () {
     return view('history2');
